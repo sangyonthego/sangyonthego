ebc88a2cd69bb33d79a971526f3ede00694cedd3
sangyonthego <sangyonthego@gmail.com>
2025-04-10 18:08:05 +0530

    Update nav bar and about

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+  const toggle = document.querySelector('.nav-toggle');
+  const links = document.querySelector('.nav-links');
+
+  toggle.addEventListener('click', () => {
+    links.classList.toggle('active');
+  });

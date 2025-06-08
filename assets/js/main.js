290cdaffac80097061048a3e2ebf5e96a8cd373e
sangyonthego <sangyonthego@gmail.com>
2025-06-08 17:57:44 +0530

    Updated toggle logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,14 @@ fetch("/header.html")
   .then(res => res.text())
   .then(data => {
     document.getElementById("site-header").innerHTML = data;
-  });
-
-  
-  
-  
-  const toggle = document.querySelector('.nav-toggle');
+  })
+  .then(  test=>{
+    const toggle = document.querySelector('.nav-toggle');
   const links = document.querySelector('.nav-links');
 
   toggle.addEventListener('click', () => {
     links.classList.toggle('active');
+
   });
+  })
+
